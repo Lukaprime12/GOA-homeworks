5f692e32b766e38b5f26dd4235d23ea1ab949604
Lukaprime12 <cxovreba34@gmail.com>
2024-03-10 14:11:10 +0400

    commit

---
+++ Day 10/Homework/hw 4.py
@@ -0,0 +1,4 @@
+for i in range(0 , 11) :
+    print(i ,"GOA IS THE BEST")
+
+    print("GOA IS G.O.A.T greatest of all time")
---
+++ Day 10/Homework/hw 5.py
@@ -0,0 +1,32 @@
+#we are gonna do some boolean stuff
+
+num_1=20
+num_2=21
+
+print(num_1 >= num_2)
+print(num_1 == num_2)
+print(num_1 <= num_2)
+print(num_1 < num_2)
+
+
+
+
+
+num_3=138
+num_4=256
+
+print(num_3 >= num_4)
+print(num_3 == num_4)
+print(num_3 <= num_4)
+print(num_3 < num_4)
+
+
+
+
+num_5=127
+num_6=100
+
+print(num_5 >= num_6)
+print(num_5 <= num_6)
+print(num_5 > num_6)
+print(num_5 < num_6)
